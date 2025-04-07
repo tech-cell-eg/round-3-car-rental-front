0e968a1764b593fcf398080c3b5ad80875869483
moelsayed011 <me6758478@gmail.com>
2025-04-07 23:04:12 +0200

    Create Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import './App.css'
+// import './App.css'
+import Footer from './components/Footer'
 
 function App() {
 
-  return (
-    <h1 className="text-red-500">Tailwind Test!</h1>
-  )
+  return <>
+  
+    {/* <h1 className="text-red-500">Tailwind Test!</h1> */}
+    <Footer />
+  </> 
+  
 }
 
 export default App
---
+++ src/components/FooterBottom.tsx
@@ -0,0 +1,30 @@
+
+const FooterBottom = () => {
+    interface Link {
+        legal: string[]
+    }
+
+    const links: Link = {
+        legal: [
+            "Privacy & Policy",
+            "Terms & Conditions"
+        ]
+    };
+
+
+    const currentYear : number = new Date().getFullYear();
+    return (
+        <div className="flex flex-col-reverse md:flex-row  justify-between gap-4 mt-7  text-gray-500 text-sm sm:text-base">
+            <div>
+                <p >© {currentYear} MORENT. All rights reserved.</p>
+            </div>
+            <div className="flex gap-10 justify-between ">
+                {links.legal.map((item, index) => (
+                    <p key={index}>{item}</p>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default FooterBottom
