6e2599085dae9868183e811355522bf0c5ac5b0b
AhmedEssam <ae.devverse@gmail.com>
2025-04-08 23:31:46 +0200

    Style: Adjust navbar size and position on different screen sizes

--- src/components/FooterBottom.tsx
+++ src/components/FooterBottom.tsx
@@ -1,30 +1,26 @@
-
 const FooterBottom = () => {
-    interface Link {
-        legal: string[]
-    }
+  interface Link {
+    legal: string[];
+  }
 
-    const links: Link = {
-        legal: [
-            "Privacy & Policy",
-            "Terms & Conditions"
-        ]
-    };
+  const links: Link = {
+    legal: ["Privacy & Policy", "Terms & Conditions"],
+  };
 
+  const currentYear: number = new Date().getFullYear();
+  return (
+    <div className="flex flex-col-reverse md:flex-row  justify-between gap-4 mt-7  text-gray-500 text-sm sm:text-base">
+      <div>
+        <p>© {currentYear} MORENT. All rights reserved.</p>
+      </div>
 
-    const currentYear : number = new Date().getFullYear();
-    return (
-        <div className="flex flex-col-reverse md:flex-row  justify-between gap-4 mt-7  text-gray-500 text-sm sm:text-base">
-            <div>
-                <p >© {currentYear} MORENT. All rights reserved.</p>
-            </div>
-            <div className="flex gap-10 justify-between ">
-                {links.legal.map((item, index) => (
-                    <p key={index}>{item}</p>
-                ))}
-            </div>
-        </div>
-    )
-}
+      <div className="flex gap-10 justify-between ">
+        {links.legal.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default FooterBottom
+export default FooterBottom;
