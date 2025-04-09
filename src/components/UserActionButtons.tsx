77787bbecad19489f1ee94f943fd8664044d9fea
AhmedEssam <ae.devverse@gmail.com>
2025-04-09 14:04:25 +0200

    Create UserActionButtons component

---
+++ src/components/UserActionButtons.tsx
@@ -0,0 +1,38 @@
+import Icon from "./Icon";
+
+type Action = {
+  id: number;
+  icon: "heart" | "notification" | "settings";
+};
+
+interface UserActionButtonsProps {
+  isMenuOpen: boolean;
+}
+
+function UserActionButtons({ isMenuOpen }: UserActionButtonsProps) {
+  const actions: Action[] = [
+    { id: 1, icon: "heart" },
+    { id: 2, icon: "notification" },
+    { id: 3, icon: "settings" },
+  ];
+
+  return (
+    <div
+      className={`${
+        isMenuOpen ? "flex" : "hidden"
+      } md:flex gap-3 md:gap-5 items-center`}
+    >
+      {actions.map((action) => (
+        <button
+          key={action.id}
+          type="button"
+          className="flex p-[5px] md:p-[10px] md:border border-gray-300 rounded-full cursor-pointer"
+        >
+          <Icon name={action.icon} />
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default UserActionButtons;
