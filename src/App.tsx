fe4a276e1721e28ef755d7fe9282230c664b09e2
AhmedEssam <ae.devverse@gmail.com>
2025-04-09 23:32:05 +0200

    add required components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // import './App.css'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
     <>
       <Header />
+      <HeroSection />
       <Footer />
     </>
   );
