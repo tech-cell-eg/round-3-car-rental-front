060a9f764c05ea9554a9ac63c7f9ad235ea666e8
moelsayed011 <me6758478@gmail.com>
2025-04-10 18:22:28 +0200

    refactor:changed types and improve code readability

--- src/components/TimePicker.tsx
+++ src/components/TimePicker.tsx
@@ -1,11 +1,11 @@
 import { Dropdown } from "primereact/dropdown"
 import { TimePickerProps } from "../types"
 
-const TimePicker: React.FC<TimePickerProps> = ({label,options, value, onChange, placeholder}) => {
+const TimePicker: React.FC<TimePickerProps> = ({label,options, value, onChange, placeholder, className}) => {
     return <>
         <div className="flex flex-col gap-2 ">
             <label htmlFor="location" className="text-sm font-medium text-gray-700 ">{label}</label>
-            <Dropdown id="location" value={value} onChange={(e) => onChange(e.value)} options={options} placeholder={placeholder} className="w-full rounded-md bg-gray-100 border text-gray-500 shadow-none  border-none focus:border-none focus:ring-0" />      
+            <Dropdown id="location" value={value} onChange={(e) => onChange(e.value)} options={options} placeholder={placeholder} className={`text-gray-500 shadow-none border-none focus:border-none focus:ring-0 ${className}`} />      
         </div>
     </> 
 }
