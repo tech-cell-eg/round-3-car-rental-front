55e0df1d0d84162c6c172825ab414b40d31e30c4
AhmedEssam <ae.devverse@gmail.com>
2025-04-13 00:38:36 +0200

    Feat: Make the logo redirectes to home upon click on it using Link component from 'react-router-dom'

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/Logo.png";
+import ROUTES from "../routes/routes";
 
 function Logo() {
   return (
-    <div className="w-[108px] md:w-fit">
+    <Link to={ROUTES.HOME} className="w-[108px] md:w-fit">
       <img src={logo} alt="logo image" className="w-full" />
-    </div>
+    </Link>
   );
 }
 
