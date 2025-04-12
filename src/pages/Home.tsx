bc8c729b92fcb1463e92b25986c552e42d6b6ef6
AhmedEssam <ae.devverse@gmail.com>
2025-04-12 19:31:44 +0200

    Chore: Change PopularCarSection name into TitledCarsSection and add props to it

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,20 @@
 import HeroSection from "../components/HeroSection";
-import PopularCarsSection from "../components/PopularCarsSection";
+import TitledCarsSetion from "../components/TitledCarsSection";
 
 function Home() {
   return (
     <section className="bg-surface-secondary">
       <HeroSection />
-      <PopularCarsSection count={4} headingValue="Popular Cars" />
-      <PopularCarsSection count={12} headingValue="Recommendation Cars" />
+      <TitledCarsSetion
+        count={4}
+        headingTitle="Popular Cars"
+        gridCols="md:grid-cols-3"
+      />
+      <TitledCarsSetion
+        count={12}
+        headingTitle="Recommendation Cars"
+        gridCols="md:grid-cols-4"
+      />
     </section>
   );
 }
