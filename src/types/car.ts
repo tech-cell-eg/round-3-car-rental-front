116679e563de3c3072c18beede5f11cff560aeae
AhmedEssam <ae.devverse@gmail.com>
2025-04-17 00:27:59 +0200

    Feat: Create car type

---
+++ src/types/car.ts
@@ -0,0 +1,11 @@
+export type Car = {
+  id: number;
+  name: string;
+  image: string;
+  type: string;
+  gasoline: number;
+  seats: string;
+  steering: string;
+  price: string;
+  sale_price: string | null;
+};
