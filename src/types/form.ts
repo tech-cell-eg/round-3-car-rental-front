85b7814f7686a634b2d356f4f28a10df93565925
moelsayed011 <me6758478@gmail.com>
2025-04-20 15:04:34 +0200

    chore:add types fot payment form

---
+++ src/types/form.ts
@@ -0,0 +1,18 @@
+export type FormValues = {
+  car_id: number;
+  name: string;
+  phone: string;
+  address: string;
+  city: string;
+  pickUpLocation: string;
+  dropOffLocation: string;
+  pickUpDate: string;
+  dropOffDate: string;
+  pickUpTime: string;
+  dropOffTime: string;
+  cardNumber: string;
+  expiryDate: string;
+  cardHolder: string;
+  cvc: string;
+  termsAccepted: boolean;
+};
