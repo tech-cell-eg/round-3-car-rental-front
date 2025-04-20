61d12080623068b78b3e14e94e416dc959b26386
moelsayed011 <me6758478@gmail.com>
2025-04-20 15:22:16 +0200

    chore:add pass api for review , car detail and payment

--- src/constants/apiEndpoints.ts
+++ src/constants/apiEndpoints.ts
@@ -4,10 +4,17 @@ interface CarApiEndpoints {
   all: string;
   recent: string;
   recommended: string;
+  carDetails: string;
+  reviewCar: string;
+}
+
+interface PaymentApiEndpoints {
+  payment: string;
 }
 
 interface ApiEndpoints {
   cars: CarApiEndpoints;
+  payment: PaymentApiEndpoints;
 }
 
 const apiEndpoints: ApiEndpoints = {
@@ -15,6 +22,11 @@ const apiEndpoints: ApiEndpoints = {
     all: `${API_BASE_URL}/cars`,
     recent: `${API_BASE_URL}/recent-cars`,
     recommended: `${API_BASE_URL}/recommended-cars`,
+    carDetails: `${API_BASE_URL}/car-details/{id}`,
+    reviewCar: `${API_BASE_URL}/car-reviews/{id}`,
+  },
+  payment: {
+    payment: `${API_BASE_URL}/rent/checkout`,
   },
 };
 
