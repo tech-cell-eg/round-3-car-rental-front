3550019db15b7694fe95fb46987e6231123b269c
moelsayed011 <me6758478@gmail.com>
2025-04-20 14:58:11 +0200

    chore:add toast provider in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 
+import  { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <>
-
           <RouterProvider router={router} />
-
+          <Toaster />
     </>
   );
 }
