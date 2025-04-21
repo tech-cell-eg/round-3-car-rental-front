edc1fcafc0dc444a2926fd88c2266f54e17e9007
AhmedEssam <ae.devverse@gmail.com>
2025-04-21 16:46:04 +0200

    Style: Change background color into white according to the design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,13 @@
-
 import FooterBottom from "./FooterBottom";
 import FooterTop from "./FooterTop";
 
 const Footer = () => {
-
-    return (
-        
-            <footer className="bg-gray-100 p-6 sm:p-8 md:p-[60px]">
-                <FooterTop  />
-                <FooterBottom  />
-            </footer>
-        
-    );
+  return (
+    <footer className="bg-surface-default p-6 sm:p-8 md:p-[60px]">
+      <FooterTop />
+      <FooterBottom />
+    </footer>
+  );
 };
 
 export default Footer;
